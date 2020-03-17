e0de06f8d4c6cd0d6c2443983a5852fee83da524
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-17 20:37:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,11 @@
 class Transfer
   
+  attr_accessor :sender, :reciever, :status, :transfer_amount 
   
   def initialize
     @sender = sender
+    @reciever = reveiver 
+    @status = "pending"
+    @transer_amount = transer_amount
+  end
 end
