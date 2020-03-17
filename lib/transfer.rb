1c386c0bf76e73165349426b694175950deafb59
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-17 20:58:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,8 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.valid? && @receiver.valid? 
-      @sender.balance > @amount && @status == "pending"
+    if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
