1f24229d9dd13466c959fc19bf399ab023a27955
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-17 21:06:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,6 +28,7 @@ class Transfer
     if @status == "complete"
       @receiver.balance -= @amount 
       @sender.balance += @amount 
+      @status = "reversed"
     else 
       @status = "rejected"
     end
